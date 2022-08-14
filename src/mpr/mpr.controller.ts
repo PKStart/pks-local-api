0a48dac0a82c68e2d82d56e07b6b6143aa17fbbf
Peter Kin <kinpeter85@gmail.com>
2022-08-14 20:52:36 +0200

    Implement test endpoint for mpr

--- src/mpr/mpr.controller.ts
+++ src/mpr/mpr.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common'
 import { MprService } from './mpr.service'
 import { ApiTags } from '@nestjs/swagger'
+import { CurrentProfile } from 'pks-common'
 
 @ApiTags('Linux / MPR')
 @Controller('linux/mpr')
@@ -8,7 +9,7 @@ export class MprController {
   constructor(private readonly mprService: MprService) {}
 
   @Get('/current-profile')
-  async getCurrentProfile(): Promise<string> {
+  async getCurrentProfile(): Promise<CurrentProfile> {
     return this.mprService.getCurrentProfile()
   }
 }
--- src/mpr/mpr.service.ts
+++ src/mpr/mpr.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common'
+import { execSync } from 'child_process'
+import { readFileSync } from 'fs'
+import { homedir } from 'os'
+import { resolve } from 'path'
+import { CurrentProfile, MprConfig } from 'pks-common'
 
 @Injectable()
 export class MprService {
-  async getCurrentProfile(): Promise<string> {
-    return 'currentProfile'
+  async getCurrentProfile(): Promise<CurrentProfile> {
+    const profiles = (
+      JSON.parse(
+        readFileSync(resolve(homedir(), process.env.PK_MPR_CONFIG_FILE)).toString()
+      ) as MprConfig
+    ).profiles
+    const activeProfile = Number(
+      readFileSync(resolve(homedir(), process.env.PK_MPR_G502_ACTIVE_PROFILE_FILE))
+        .toString()
+        .trim()
+    )
+    const gameProfile = readFileSync(resolve(homedir(), process.env.PK_MPR_G502_GAME_PROFILE_FILE))
+      .toString()
+      .trim()
+
+    const output = execSync(`/bin/bash ${resolve(homedir(), process.env.PK_MPR_FOLDER, 'mpr.sh')}`)
+    console.log(output.toString().trim() || 'no output')
+
+    return {
+      activeProfile,
+      configuredGame: profiles.find(({ name }) => name === gameProfile),
+    }
   }
 }
